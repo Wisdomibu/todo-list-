bea656f31047e17171b14bd4df903e58bc2b643f
Wisdom Ibu <wisdomibu@gmail.com>
2023-10-20 14:24:12 +0100

    fixed linter error

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,43 @@
-const path = require('path')
-const HtmlWebpackPlugin = require ('html-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js')}
-        ,
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-        clean: true,
+  mode: 'development',
+  entry: { bundle: path.resolve(__dirname, 'src/index.js') },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
-    devServer:{
-        static: {
-            directory: path.resolve(__dirname, 'dist')
-        },
-        port:3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-              test:/\.scss$/,
-              use:[
-                'style-loader', 
-                'css-loader',
-                'sass-loader'
-              ]
-            },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin(
-            {
-                title: 'webpack App',
-                filename: 'index.html',
-                template: 'src/index.html'
-            }
-        )
-    ]
-}
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        title: 'webpack App',
+        filename: 'index.html',
+        template: 'src/index.html',
+      },
+    ),
+  ],
+};
